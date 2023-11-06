92b83a65e1396346923ae24e17686aa4cd614144
EduardoAlparov <workman133@yandex.ru>
2023-11-06 11:33:35 +0300

    forms fixx 666.10.23

--- src/js/modules/dragMapSearch.js
+++ src/js/modules/dragMapSearch.js
@@ -5,23 +5,6 @@ export default () => {
     const btn = document.getElementById('drag-list-btn');
     const menu = document.getElementById('map-search-list');
 
-    const debounce = (func, wait, immediate) => {
-      var timeout;
-
-      return function () {
-        var context = this,
-            args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        callNow && func.apply(context, args);
-      };
-    }
-
     function DragMenu (options) {
       const self    = this;
       self.menu     = options.el;
@@ -60,16 +43,16 @@ export default () => {
         var l = parseInt(menu.style.bottom);
       });
 
-      self.menu.addEventListener(self.eventMove, debounce(e => {
+      self.menu.addEventListener(self.eventMove, e => {
         self.menu.classList.add('is-moving');
         var evt = e.type === 'touchmove' ? e.changedTouches[0] : e;
         var move = (window.innerHeight - (evt.clientY)) - self.position.current;
 
-        if ((move) >= self.position.min && (move) <= self.position.max) {
+        if ((move) < self.position.max && (move) >= self.position.min) {
           self.setOpen(false);
           self.menu.style.bottom = `${move}px`;
         }
-      }), 200);
+      });
 
       self.menu.addEventListener(self.eventEnd, e => {
         self.menu.classList.remove('is-moving');
@@ -78,11 +61,7 @@ export default () => {
 
         if( Math.abs(l) > self.position.snapBorder) {
           self.setOpen(true);
-        } else {
-          self.setOpen(false);
-          self.menu.classList.remove('is-moving');
         }
-
         self.menu.style.bottom = null;
       });
 
