3067cebac925ebe32a2e67f068566e01e965652b
sevastyan <foxhable@gmail.com>
2024-02-28 17:29:12 +0300

    fix: opening several filter items at the same time

--- src/js/modules/footerAcco.js
+++ src/js/modules/footerAcco.js
@@ -1,21 +1,7 @@
 export default () => {
-  const btns = document.querySelectorAll('.footer-menu__button:not(.mobile-menu__link, .js-catalog-next-slide)');
+  const btns = document.querySelectorAll('.footer-menu__button:not(.mobile-menu__link, .js-catalog-next-slide)')
 
-  if(btns.length > 0) {
-    Array.from(btns).forEach( btn => {
-      btn.addEventListener('click', () => {
-        if(btn.classList.contains('footer-menu__button--opened')) {
-          Array.from(btns).forEach( b => {
-            b.classList.remove('footer-menu__button--opened');
-          })
-        } else {
-          Array.from(btns).forEach( b => {
-            b.classList.remove('footer-menu__button--opened');
-          })
-
-          btn.classList.add('footer-menu__button--opened');
-        }
-      })
-    })
-  }
+  btns.forEach(btn => {
+    btn.addEventListener('click', () => btn.classList.toggle('footer-menu__button--opened'))
+  })
 }
