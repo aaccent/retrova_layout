87810678797826c0b773afd9f0e4203a631d69f1
Regina <rmyachina@mail.ru>
2024-05-15 11:40:33 +0300

    fix: header menu

--- src/js/modules/hamburgerControl.js
+++ src/js/modules/hamburgerControl.js
@@ -1,13 +1,13 @@
 export default () => {
   let activeSubmenu = null
 
-  const body = document.body;
-  const header = document.querySelector('.header');
-  const headerMenu = document.querySelector('.header__menu');
-  const burgerBtn = document.querySelector('.hamburger-btn');
+  const body = document.body
+  const header = document.querySelector('.header')
+  const headerMenu = document.querySelector('.header__menu')
+  const burgerBtn = document.querySelector('.hamburger-btn')
   const headerCatalog = document.querySelector('.header__catalog')
 
-  if(!burgerBtn) return;
+  if (!burgerBtn) return
 
   function closeMobileMenus() {
     headerCatalog?.classList.remove('_active')
@@ -16,9 +16,9 @@ export default () => {
   }
 
   burgerBtn.addEventListener('click', () => {
-    body.classList.toggle('menu-is-open');
-    burgerBtn.classList.toggle('hamburger-btn--open');
-    headerMenu.classList.toggle('_active');
+    body.classList.toggle('menu-is-open')
+    burgerBtn.classList.toggle('hamburger-btn--open')
+    headerMenu.classList.toggle('_active')
     headerMenu.style.height = `calc(100dvh - ${header.offsetHeight}px)`
 
     if (!body.classList.contains('menu-is-open')) closeMobileMenus()
@@ -28,22 +28,26 @@ export default () => {
   const mobileMenuFooter = document.querySelector('.mobile-menu__footer')
 
   itemWithSubitems.forEach(item => {
-    const button = item.parentElement.querySelector('.menu__link')
-    button.onclick = () => {
-      item.style.top = `${header.offsetHeight}px`
-      item.style.height = `calc(100dvh - ${header.offsetHeight}px)`
+    if (window.innerWidth <= 1200) {
+      console.log('kjgljlkg')
+      const button = item.parentElement.querySelector('.menu__link')
+      button.onclick = () => {
+        item.style.top = `${header.offsetHeight}px`
+        item.style.height = `calc(100dvh - ${header.offsetHeight}px)`
 
-      activeSubmenu?.classList.remove('_active')
-      item.classList.add('_active')
-      activeSubmenu = item
+        activeSubmenu?.classList.remove('_active')
+        item.classList.add('_active')
+        activeSubmenu = item
+      }
+
+
+      const backButton = document.createElement('button')
+      backButton.className = 'menu__back-button'
+      backButton.innerHTML = `<span>${button.textContent.trim()}</span>`
+      backButton.onclick = closeMobileMenus
+
+      item.prepend(backButton)
+      item.append(mobileMenuFooter.cloneNode(true))
     }
-
-    const backButton = document.createElement('button')
-    backButton.className = 'menu__back-button'
-    backButton.innerHTML = `<span>${button.textContent.trim()}</span>`
-    backButton.onclick = closeMobileMenus
-
-    item.prepend(backButton)
-    item.append(mobileMenuFooter.cloneNode(true))
   })
 }
